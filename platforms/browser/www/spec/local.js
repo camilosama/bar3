f155688a1e80c0bedbc126207a9342ac69dc6f12
Andres Castellanos <camilosama64@MacBook-Pro-de-Andres.local>
2019-11-21 23:23:02 -0500

    all

---
+++ platforms/browser/www/spec/local.js
@@ -0,0 +1,7 @@
+function verificarFormulario(){
+	alert($('input:radio[name=opc1]:checked').val());
+	alert($('input:radio[name=opc2]:checked').val());
+	alert($('input:radio[name=opc3]:checked').val());
+	alert($('input:radio[name=opc4]:checked').val());
+	alert($('input:radio[name=opc5]:checked').val());
+}
